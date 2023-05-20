e11080b4d73229f23aad27f73371ee1a9e4ed071
PatriNet77 <131683643+PatriNet77@users.noreply.github.com>
2023-05-20 17:16:04 -0300

    Update Desafio4.py

--- Semana4/Desafio4.py
+++ Semana4/Desafio4.py
@@ -120,7 +120,11 @@ def buscar_presupuesto(lista):
     return inmuebles_encontrados
 
 # Lista de inmuebles
-inmuebles = []
+inmuebles = [{"año": 2010, "metros": 150, "habitaciones": 4, "garaje": True, "zona": "C", "estado": "Disponible"},
+{"año": 2016, "metros": 80, "habitaciones": 2, "garaje": False, "zona": "B", "estado": "Reservado"},
+{"año": 2000, "metros": 180, "habitaciones": 4, "garaje": True, "zona": "A", "estado": "Disponible"},
+{"año": 2015, "metros": 95, "habitaciones": 3, "garaje": True, "zona": "B", "estado": "Vendido"},
+{"año": 2008, "metros": 60, "habitaciones": 2, "garaje": False, "zona": "C", "estado": "Disponible"}]
 
 # Programa principal
 while True:
