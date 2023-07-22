0c55c0d0ca5d745a5cc1df6567d25226b766368a
Jorge Gimenez <jorge.2015.8893@gmail.com>
2023-07-22 00:59:55 -0300

    Creando ENTORNO Blog-AAA
    Creamdo entorno de trabajo.

--- Semana13/blog/blog/configuraciones/settings.py
+++ Semana13/blog/blog/configuraciones/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-^4%ygvc(*jcg1q0@v^fjnuii=%1^gjh@)l6+6zwtfh69cytohq
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
