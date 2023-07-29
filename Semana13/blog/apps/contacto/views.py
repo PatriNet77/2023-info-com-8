96b55dbe7a93fca51cb050d0ebc5b7ebf1ad1ac8
Jorge Gimenez <jorge.2015.8893@gmail.com>
2023-07-29 14:53:00 -0300

    App Contacto
    Se cambió una dirección de manera de probar.

--- Semana13/blog/apps/contacto/views.py
+++ Semana13/blog/apps/contacto/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse_lazy
 class ContactoUsuario(CreateView):
     template_name = 'contacto/contacto.html'
     form_class = ContactoForm
-    success_url = reverse_lazy('index')
+    #success_url = reverse_lazy('index')
+    success_url = reverse_lazy('apps.contacto:contacto')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
